4c0d41f9859f103f61b3d55281c53386521f8120
Himanshu <himanshuchhokar131@gmail.com>
2026-08-22 21:39:25 +0530

    Time: 397 ms (6.41%), Space: 42.4 MB (98.28%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,65 @@
+class Solution {
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+
+        for (int row = 0; row < 9; row++) {
+
+            for (int col = 0; col < 9; col++) {
+
+                if (board[row][col] == '.') {
+
+                    for (char num = '1'; num <= '9'; num++) {
+
+                        if (isValid(board, row, col, num)) {
+
+                            board[row][col] = num;
+
+                            if (solve(board)) {
+                                return true;
+                            }
+
+                            // Backtrack
+                            board[row][col] = '.';
+                        }
+                    }
+
+                    // No valid number found
+                    return false;
+                }
+            }
+        }
+
+        // Puzzle solved
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char num) {
+
+        for (int i = 0; i < 9; i++) {
+
+            // Check row
+            if (board[row][i] == num) {
+                return false;
+            }
+
+            // Check column
+            if (board[i][col] == num) {
+                return false;
+            }
+
+            // Check 3x3 box
+            int newRow = 3 * (row / 3) + i / 3;
+            int newCol = 3 * (col / 3) + i % 3;
+
+            if (board[newRow][newCol] == num) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
