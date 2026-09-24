63621617fd8dc18bdb1b28e30347a730a765453f
Himanshu <himanshuchhokar131@gmail.com>
2026-09-24 20:17:49 +0530

    Time: 0 ms (100%), Space: 44.8 MB (89.64%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private boolean validate(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+
+        // Node must be strictly between min and max
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+
+        // Left subtree: values must be smaller than node.val
+        // Right subtree: values must be greater than node.val
+        return validate(node.left, min, node.val)
+            && validate(node.right, node.val, max);
+    }
+}
